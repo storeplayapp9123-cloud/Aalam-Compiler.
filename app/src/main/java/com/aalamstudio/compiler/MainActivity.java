9a24f5ca74e6ada301668accae544d01d77fb8de
storeplayapp9123-cloud <storeplayapp9123@gmail.com>
2026-09-13 18:07:50 +0530

    Update MainActivity.java

--- app/src/main/java/com/aalamstudio/compiler/MainActivity.java
+++ app/src/main/java/com/aalamstudio/compiler/MainActivity.java
@@ -1,6 +1,8 @@
 package com.aalamstudio.compiler;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buildButton = findViewById(R.id.buildButton);
+        Button settingsButton = findViewById(R.id.settingsButton);
+
+        buildButton.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, BuildApkActivity.class));
+        });
+
+        settingsButton.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+        });
     }
 }
